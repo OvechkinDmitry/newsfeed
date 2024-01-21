4474952ed4cf5815292334a8a223de7f6cfadca9
DmitryOvechkin <ovechkin_ds@ekb.sima-land.ru>
2024-01-21 22:13:37 +0500

    added base routing and code splitting

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,20 @@
 import {Counter} from "./components/counter";
+import {Routes, Route, Link} from 'react-router-dom'
 import './index.scss'
+import {AsyncAbout} from "./pages/about/async";
+import {AsyncMain} from "./pages/main/async";
+import {Suspense} from "react";
 
 export const App = () => {
     return <div className='app'>
+        <Link to={'/about'} children={'About'}/>
+        <Link to={'/'} children={'Main'}/>
+        <Suspense fallback={null}>
+            <Routes>
+                <Route path={'/about'} element={<AsyncAbout/>}/>
+                <Route path={'/'} element={<AsyncMain/>}/>
+            </Routes>
+        </Suspense>
         <Counter/>
     </div>
 }
