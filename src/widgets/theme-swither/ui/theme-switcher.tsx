6faf2c47e1e1e29b7652834e26a840aecba54733
DmitryOvechkin <ovechkin_ds@ekb.sima-land.ru>
2024-01-30 01:23:15 +0500

    added style lint

--- src/widgets/theme-swither/ui/theme-switcher.tsx
+++ src/widgets/theme-swither/ui/theme-switcher.tsx
@@ -3,7 +3,6 @@ import { cx } from 'shared/lib/class-names';
 import { Theme, useTheme } from 'app/providers/theme';
 import { Button } from 'shared/ui/button';
 import { IoIosSunny as SunIcon, IoMdMoon as MoonIcon } from 'react-icons/io';
-import styles from './theme-switcher.module.scss';
 
 interface Props{
     className?: string;
@@ -13,7 +12,7 @@ function ThemeSwitcher(props: Props) {
   const { theme, toggleTheme } = useTheme();
   const { className } = props;
   return (
-    <Button className={cx(styles.root, className)} onClick={toggleTheme}>
+    <Button className={cx(className)} onClick={toggleTheme}>
       {theme === Theme.LIGHT ? <SunIcon size={24} /> : <MoonIcon size={24} />}
     </Button>
   );
