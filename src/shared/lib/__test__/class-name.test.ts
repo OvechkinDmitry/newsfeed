f5885c70ffcf8459557f9b973fbfe3c37e108781
DmitryOvechkin <ovechkin_ds@ekb.sima-land.ru>
2024-01-31 01:00:50 +0500

    added jest config

---
+++ src/shared/lib/__test__/class-name.test.ts
@@ -0,0 +1,11 @@
+import { cx } from '../class-names';
+
+describe('cx', () => {
+  it('should work with strings', () => {
+    expect(cx('class1', 'class2')).toEqual('class1 class2');
+  });
+
+  it('should work with incorrect params', () => {
+    expect(cx('class1', undefined, 'class2')).toEqual('class1 class2');
+  });
+});
